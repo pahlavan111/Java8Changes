136d6634f21b86e049d9d64030753d1199c95352
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-06 19:51:04 +0430

    using Functional interface in main activity and make lambda expression instead overriding its method

--- app/src/main/java/com/bp/java8changes/MainActivity.java
+++ app/src/main/java/com/bp/java8changes/MainActivity.java
@@ -3,8 +3,11 @@ package com.bp.java8changes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements InterfaceWithDefaultMethod{
+public class MainActivity extends AppCompatActivity implements InterfaceWithDefaultMethod {
+    private static final String TAG_KEY = "behrooz_log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +15,34 @@ public class MainActivity extends AppCompatActivity implements InterfaceWithDefa
         setContentView(R.layout.activity_main);
 
         this.b();
+
+        // 3 type of using interface
+        //--1
+        MyFunctionalInterface mi = new MyFunctionalInterface() {
+            @Override
+            public void myMethod() {
+                Log.d(TAG_KEY, "this is a massage from my method");
+            }
+        };
+        mi.myMethod();
+
+        //--2
+        MyFunctionalInterface mi2 = () -> {
+            Log.d(TAG_KEY, "this is a massage from my method mi2");
+        };
+        mi2.myMethod();
+
+        //--3
+        // if you have just one line no need to curly braces
+        MyFunctionalInterface mi3 = () -> Log.d(TAG_KEY, "this is a massage from my method mi3");
+        mi3.myMethod();
+
+
     }
 
     @Override
     public void a() {
 
     }
+
 }
