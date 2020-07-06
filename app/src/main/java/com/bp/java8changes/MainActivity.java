7e77e3c0bfe8bced3586ba0c6556573cce4aca38
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-06 20:24:41 +0430

    use MyFunctionalInterface2 in MainActivity

--- app/src/main/java/com/bp/java8changes/MainActivity.java
+++ app/src/main/java/com/bp/java8changes/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements InterfaceWithDefaultMethod {
     private static final String TAG_KEY = "behrooz_log";
@@ -16,27 +15,37 @@ public class MainActivity extends AppCompatActivity implements InterfaceWithDefa
 
         this.b();
 
+
+        //-----------------------------------------------------------------------------------------------
         // 3 type of using interface
         //--1
-        MyFunctionalInterface mi = new MyFunctionalInterface() {
+        MyFunctionalInterface mfi1 = new MyFunctionalInterface() {
             @Override
             public void myMethod() {
                 Log.d(TAG_KEY, "this is a massage from my method");
             }
         };
-        mi.myMethod();
+        mfi1.myMethod();
 
         //--2
-        MyFunctionalInterface mi2 = () -> {
-            Log.d(TAG_KEY, "this is a massage from my method mi2");
+        MyFunctionalInterface mfi2 = () -> {
+            Log.d(TAG_KEY, "this is a massage from my method mfi2");
         };
-        mi2.myMethod();
+        mfi2.myMethod();
 
         //--3
         // if you have just one line no need to curly braces
-        MyFunctionalInterface mi3 = () -> Log.d(TAG_KEY, "this is a massage from my method mi3");
-        mi3.myMethod();
+        MyFunctionalInterface mfi3 = () -> Log.d(TAG_KEY, "this is a massage from my method mfi3");
+        mfi3.myMethod();
+//---------------------------------------------------------------------------------------------------------
 
+
+        MyFunctionalInterface2 mfi4 = str -> Log.d(TAG_KEY,str);
+        mfi4.Method("hey I am from mfi4 with a String Argument");
+
+        //or
+        MyFunctionalInterface2 mfi5 = (s) -> Log.d(TAG_KEY,s);
+        mfi5.Method("hey I am from mfi5 with a String Argument");
 
     }
 
